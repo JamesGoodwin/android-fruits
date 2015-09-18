35daac1ef6d130d02affa2506f53b4cb915b2a72
James Goodwin <jamesgoodwin@gmail.com>
2015-09-18 09:04:18 +0100

    Use Presenter, remove unnecessary Loader

--- app/src/main/java/com/jgoodwin/ustwo/activity/FruitListFragment.java
+++ app/src/main/java/com/jgoodwin/ustwo/activity/FruitListFragment.java
@@ -1,8 +1,6 @@
-package com.jgoodwin.ustwo;
+package com.jgoodwin.ustwo.activity;
 
 import android.app.Fragment;
-import android.app.LoaderManager;
-import android.content.Loader;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
@@ -11,15 +9,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.jgoodwin.ustwo.FruitsApp;
+import com.jgoodwin.ustwo.R;
+import com.jgoodwin.ustwo.domain.Fruit;
+
 import java.util.List;
+
+import javax.inject.Inject;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class FruitListFragment extends Fragment implements LoaderManager.LoaderCallbacks<List<Fruit>> {
+public class FruitListFragment extends Fragment implements FruitsView {
 
     @Bind(R.id.list)
     RecyclerView list;
+
+    @Inject
+    FruitsPresenter presenter;
 
     @Nullable
     @Override
@@ -28,30 +35,27 @@ public class FruitListFragment extends Fragment implements LoaderManager.LoaderC
 
         ButterKnife.bind(this, view);
 
+        FruitsApp.getComponent().inject(this);
+
         return view;
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        getLoaderManager().initLoader(this.getClass().hashCode(), null, this).forceLoad();
+        presenter.showFruits(this);
     }
 
     @Override
-    public Loader<List<Fruit>> onCreateLoader(int i, Bundle bundle) {
-        return new FruitsLoader(getActivity());
-    }
-
-    @Override
-    public void onLoadFinished(Loader<List<Fruit>> loader, List<Fruit> fruits) {
+    public void showFruits(List<Fruit> fruits) {
         FruitsAdapter adapter = new FruitsAdapter(fruits);
         list.setLayoutManager(new LinearLayoutManager(getActivity()));
         list.setAdapter(adapter);
     }
 
     @Override
-    public void onLoaderReset(Loader<List<Fruit>> loader) {
+    public void showError() {
 
     }
+
 }
